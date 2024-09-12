467a12dbf5f1a1c6ed9e2684f98a81d4a0c24634
ShaguftaHussain <shagufta.hussain@cginfinity.com>
2024-09-12 15:31:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,5 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=true)
 
